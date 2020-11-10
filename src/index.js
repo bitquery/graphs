6b6189514c5cb6c9cea72405c38c33d84947723e
Andrey Ivanov <ivanov17andrey@gmail.com>
2020-11-10 03:08:34 +0300

    right click link

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import _n from 'numeral'
+
 import { Network, DataSet } from '../node_modules/vis'
 import { addInitializingLoader } from './addInitializingLoader'
 import { CurrencyFilter } from './components/CurrencyFilter'
@@ -10,6 +11,10 @@ import { addFullScreenButton } from './addFullScreenButton'
 import { lightenOrDarkenColor } from './lightenOrDarkenColor'
 import { addModalJS } from './addModalJS'
 import { addModalGraphQL } from './addModalGraphQL'
+
+// import * as d3 from 'd3'
+// import { sankey as d3Sankey, sankeyLinkHorizontal } from 'd3-sankey'
+
 import './style.scss'
 
 export function query(query) {
@@ -91,8 +96,8 @@ export function address_graph(selector, query, options) {
     .outerHTML.replace(/queryWithTimeRange\(.*\)/, `query.request({})`)
     .replace(
       /<script>/,
-      `  <link rel="stylesheet" media="all" href="https://cdn.jsdelivr.net/gh/bitquery/graphs/dist/graphs.min.css">
-  <script src="https://cdn.jsdelivr.net/gh/bitquery/graphs/dist/graphs.min.js"></script>
+      `  <link rel="stylesheet" media="all" href="https://cdn.jsdelivr.net/gh/bitquery/graphs@1/dist/graphs.min.css">
+  <script src="https://cdn.jsdelivr.net/gh/bitquery/graphs@1/dist/graphs.min.js"></script>
   <script>`
     )
 
@@ -660,6 +665,14 @@ export function address_graph(selector, query, options) {
       }
     })
 
+    g.network.on('oncontext', function(params) {
+      if (params.nodes.length > 0) {
+				let node = g.dataset.nodes.get(params.nodes[0])
+				const pathname = window.location.pathname.slice(1)
+        window.open(`${window.location.origin}/${pathname.slice(0, pathname.indexOf('/'))}/address/${node.id}`, '_blank')
+      }
+    })
+
     if (g.dataset.nodes.length == 0) return
 
     g.expandNode(query.variables.address)
@@ -688,3 +701,140 @@ export function address_graph(selector, query, options) {
   query.components.push(g)
   return g
 }
+
+// export function address_sankey(selector, data, options) {
+//   var units = 'Widgets'
+
+//   // set the dimensions and margins of the graph
+//   var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+//     width = 700 - margin.left - margin.right,
+//     height = 300 - margin.top - margin.bottom
+
+//   // format variables
+//   var formatNumber = d3.format(',.0f'), // zero decimal places
+//     format = function(d) {
+//       return formatNumber(d) + ' ' + units
+//     },
+//     color = d3.scaleOrdinal(d3.schemeCategory10)
+
+//   // append the svg object to the body of the page
+//   var svg = d3
+//     .select('body')
+//     .append('svg')
+//     .attr('width', width + margin.left + margin.right)
+//     .attr('height', height + margin.top + margin.bottom)
+//     .append('g')
+//     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+//   // Set the sankey diagram properties
+//   var sankey = d3Sankey()
+//     .nodeWidth(36)
+//     .nodePadding(40)
+//     .size([width, height])
+
+//   var path = sankeyLinkHorizontal()
+
+//   // load the data
+//   d3.json('./graph/src/sankey.json').then(function(graph) {
+//     sankey
+//       .nodes(graph.nodes)
+//       .links(graph.links)
+
+//     // add in the links
+//     var link = svg
+//       .append('g')
+//       .selectAll('.link')
+//       .data(graph.links)
+//       .enter()
+//       .append('path')
+//       .attr('class', 'link')
+//       .attr('d', path)
+//       .style('stroke-width', function(d) {
+//         return Math.max(1, d.dy)
+//       })
+//       .sort(function(a, b) {
+//         return b.dy - a.dy
+//       })
+
+//     // add the link titles
+//     link.append('title').text(function(d) {
+//       return d.source.name + ' → ' + d.target.name + '\n' + format(d.value)
+//     })
+
+//     // add in the nodes
+//     var node = svg
+//       .append('g')
+//       .selectAll('.node')
+//       .data(graph.nodes)
+//       .enter()
+//       .append('g')
+//       .attr('class', 'node')
+//       .attr('transform', function(d) {
+//         return 'translate(' + d.x + ',' + d.y + ')'
+//       })
+//       .call(
+//         d3
+//           .drag()
+//           .subject(function(d) {
+//             return d
+//           })
+//           .on('start', function() {
+//             this.parentNode.appendChild(this)
+//           })
+//           .on('drag', dragmove)
+//       )
+
+//     // add the rectangles for the nodes
+//     node
+//       .append('rect')
+//       .attr('height', function(d) {
+//         return d.dy
+//       })
+//       .attr('width', sankey.nodeWidth())
+//       .style('fill', function(d) {
+//         return (d.color = color(d.name.replace(/ .*/, '')))
+//       })
+//       .style('stroke', function(d) {
+//         return d3.rgb(d.color).darker(2)
+//       })
+//       .append('title')
+//       .text(function(d) {
+//         return d.name + '\n' + format(d.value)
+//       })
+
+//     // add in the title for the nodes
+//     node
+//       .append('text')
+//       .attr('x', -6)
+//       .attr('y', function(d) {
+//         return d.dy / 2
+//       })
+//       .attr('dy', '.35em')
+//       .attr('text-anchor', 'end')
+//       .attr('transform', null)
+//       .text(function(d) {
+//         return d.name
+//       })
+//       .filter(function(d) {
+//         return d.x < width / 2
+//       })
+//       .attr('x', 6 + sankey.nodeWidth())
+//       .attr('text-anchor', 'start')
+
+//     // the function for moving the nodes
+//     function dragmove(d) {
+//       d3.select(this).attr(
+//         'transform',
+//         'translate(' +
+//           d.x +
+//           ',' +
+//           (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) +
+//           ')'
+//       )
+//       sankey.relayout()
+//       link.attr('d', path)
+//     }
+//   })
+// }
+
+// address_sankey()
--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,8 @@ module.exports = {
   },
 
   optimization: {
-    minimize: true,
+    minimize: false,
+    // minimize: true,
     minimizer: [
       new TerserPlugin({
         parallel: true,
