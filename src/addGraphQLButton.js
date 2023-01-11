3e512950f1f4d66f1d1113036869fa79053ecaf9
Spendil <spendil@mail.ru>
2023-01-11 16:25:40 +0300

    update get api button

--- src/addGraphQLButton.js
+++ src/addGraphQLButton.js
@@ -4,34 +4,29 @@ export const addGraphQLButton = (container, query, ideUrl) => {
 
     const modalGQL = $(`
 		<div>
-			<a class="badge badge-secondary open-btn">Open GraphQL IDE</a>
+			<a class="badge badge-secondary open-btn">Get API</a>
 		</div>
 	`)
 
     container.append(modalGQL)
 
     $(modalGQL).on('click', (e) => {
-        let wwindow = window.open()
-        fetch(ideUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-            body: JSON.stringify({
-                query: query.query,
-                variables: JSON.stringify(query.variables),
-            }),
-        })
-            .then((res) => {
-                if (res.status === 200 || res.status === 302) {
-                    wwindow.location.href = res.url
-                    // window.open(res.url, '_blank').focus()
-                    // res.redirect(302, res.url)
-                } else {
-                    console.log(res.message)
-                }
-            })
+        let createHiddenField = function (name, value) {
+            let input = document.createElement('input');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', name);
+            input.setAttribute('value', value);
+            return input;
+        }
+        let form = document.createElement('form');
+        form.setAttribute('method', 'post');
+        form.setAttribute('action', ideUrl);
+        form.setAttribute('target', '_blank');
+        form.setAttribute('enctype', 'application/json');
+        form.appendChild(createHiddenField('query', JSON.stringify(query.query)));
+        form.appendChild(createHiddenField('variables', JSON.stringify(query.variables)));
+        document.body.appendChild(form);
+        form.submit();
+        document.body.removeChild(form);
     })
-
 }
